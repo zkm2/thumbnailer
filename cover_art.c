1d946435d3a8e8465cce3111ddcbda7c37ebeea9
zkm2 <zkm@chiru.no>
2026-05-20 02:00:14 -0400

    Revert "Fix more AVIODirContext errors"
    This reverts commit ab75814544656b73629db5e2bb5bf837d933d734.

--- cover_art.c
+++ cover_art.c
@@ -1,7 +1,7 @@
 #include "cover_art.h"
 
 // Extract embedded image
-AVPacket retrieve_cover_art(AVIODirContext* ctx)
+AVPacket retrieve_cover_art(AVFormatContext* ctx)
 {
     const int i = find_cover_art(ctx);
     if (i != -1) {
@@ -13,7 +13,7 @@ AVPacket retrieve_cover_art(AVIODirContext* ctx)
 }
 
 // Find the first attached picture, if available
-int find_cover_art(AVIODirContext* ctx)
+int find_cover_art(AVFormatContext* ctx)
 {
     for (int i = 0; i < ctx->nb_streams; i++) {
         const int d = ctx->streams[i]->disposition;
